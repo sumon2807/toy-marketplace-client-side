7caaa9825de4da6dfa97e17579a95d54779aaa19
sumon2807 <sharifulbd911@gmail.com>
2023-05-18 11:04:47 +0600

    install firebase and login page is ready for UI

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200 ">
+            <div className="hero-content w-[50%]">
+                <div className="card w-full shadow-2xl bg-base-100">
+                    <div className="card-body ">
+                        <form>
+                            <h2 className='text-4xl font-bold text-center'>Please Login</h2>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" name='email' placeholder="email" className="input input-bordered" required/>
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required/>
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                                <label className="label">
+                                    <Link to="/register" className="label-text-alt link link-hover">New to KIDZ? Please Register Now</Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary" type="submit" value="Login" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
